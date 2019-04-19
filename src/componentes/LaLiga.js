37016acb67320a916a04001b068ab8b8f4fb1f52
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-04-19 17:19:13 +0200

    context api optimized and working perfectly

---
+++ src/componentes/LaLiga.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+// Crear el context
+const LaLigaContext = React.createContext();
+export { LaLigaContext };
+
+class LaLigaProvider extends Component {
+
+  state = {
+    equipos : [
+      {
+        nombre: 'Real Madrid',
+        titulos: 35
+      },
+      {
+        nombre: 'Barcelona',
+        titulos: 25
+      },
+      {
+        nombre: 'Atletico de Madrid',
+        titulos: 10
+      }
+    ]
+  }
+    
+    render() { 
+        return ( 
+            <LaLigaContext.Provider value={{
+                state: this.state.equipos,
+                esCampeon: (id) => {
+                    const equipos = [...this.state.equipos];
+
+                    equipos[id].titulos = equipos[id].titulos + 1;
+                    
+                    this.setState({
+                        state : equipos
+                    })
+                }
+            }}>
+            {this.props.children}
+
+            </LaLigaContext.Provider>
+         );
+    }
+}
+ 
+export default LaLigaProvider;
